e2b4d57634cebe9e815769636702efb9b5286671
beggarprince <beatyouonetime@gmail.com>
2026-01-06 21:09:20 -0600

    Started working on ingest_kafka; My laptop is about to die

--- Assets/Jobs/ingest_kafka_to_landing.py
+++ Assets/Jobs/ingest_kafka_to_landing.py
@@ -25,9 +25,48 @@ def consume_batch(topic: str, batch_duration_sec: int, output_path: str) -> int:
         Number of messages consumed
     """
     # TODO: Implement
-    pass
+   kafka_consumer = KafkaConsumer(
+           topic, #Topic we are consuming from
+           bootstrap_servers=['localhost:9094'], #This needs to match the yaml file. Should change to var
+           enable_auto_commit=True, #idk if this is necessary
+           value_deserializer = lambda v: json.loads(v.decode('utf-8'))
+           )
 
+   #Consume for batch_duration_sec
+   batch_messages = []
+   start_time = time.time()
+   end_time = start_time + batch_duration_sec
+
+   while time.time() < end_time():
+       interval_msg = kafka_consumer.poll(timeout_ms=1000)
+
+       for partition, messages in interval_msg.items():
+           for message in messages:
+               batch_messages.append(message.value)
+
+   kafka_consumer.close()
+
+    #Export to file
+    if batch_messages:
+        #Check if dir exists and create if it doesn't
+        os.makedirs(output_path, exist_ok=True)
+        #Create file
+        timestamp = datetime.now()
+        filename = f"{topic}_batch_{timestamp}.json"
+        path= os.path.join(output_path, filename)
+
+        #Write to file
+        with open(path, 'w') as file:
+            json.dump(batch_messages, f, indent=4)
+            printf("Wrote {len{batch_messages}} to {path}")
+        except IOError as e:
+            printf("Failed to write to {path}")
+            print(e)
+            return 0
+
+    else:
+        print("There are no messages")
 
 if __name__ == "__main__":
     # TODO: Parse args and call consume_batch
-    pass
+    
---
+++ kafka/README.txt
@@ -0,0 +1,1 @@
+I forgot what i was doing here I don't remember what this file does
