27ff8890ef466239b258f7ce07976fbf400c2387
beggarprince <palvarez122@hotmail.com>
2026-01-08 19:35:15 -0600

    Removed old producers, made temp bash files to run consumer with arguments, added files with json output from kafka consumers for refernce

---
+++ Assets/user_consumer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#Optional argument of time
+# default to 10 seconds if no time argument give
+
+SECONDS=${1:-10}
+
+# Check if it's a number
+if ! [[ "$SECONDS" =~ ^[0-9]+$ ]]; then
+  echo "Usage: $0 [seconds]"
+  echo "Error: seconds must be a positive number"
+  exit 1
+fi
+
+echo "Running User Event Consumer for $SECONDS seconds..."
+python3 Jobs/ingest_kafka_to_landing.py --topic user_events
+
